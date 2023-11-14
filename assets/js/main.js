f0abb73c3164a56d6c2f672d620383b719ba91b3
ENGKhaledAbdullah <eng.khaled.alhamedi@gmail.com>
2023-11-14 12:51:44 +0300

    added small devices menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,5 +10,13 @@ $(function(){
     $("#project-categories-list3").click(function(){
         $("#project-categories-list-items3").toggle(100);
         $("#oc-arrow3").toggleClass("rotate-arrow");
-    });   
+    });
+    console.clear();
+    const list = document.querySelectorAll('.list');
+    const nav = document.querySelector('.navigation');
+    list.forEach(item => item.addEventListener('click', function(e){
+        list.forEach(li => li.classList.remove('active'));
+        e.currentTarget.classList.add('active');
+    }));   
 });
+
