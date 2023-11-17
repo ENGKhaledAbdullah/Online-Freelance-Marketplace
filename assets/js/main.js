0857b77d8ab10891c31929ece4c2a2bcbb7b3c42
ENGKhaledAbdullah <eng.khaled.alhamedi@gmail.com>
2023-11-17 20:08:25 +0300

    added sections and fixes few responsivity to the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,25 @@ $(function(){
         $("#project-categories-list-items3").toggle(100);
         $("#oc-arrow3").toggleClass("rotate-arrow");
     });
-    console.clear();
-    const list = document.querySelectorAll('.list');
-    const nav = document.querySelector('.navigation');
-    list.forEach(item => item.addEventListener('click', function(e){
-        list.forEach(li => li.classList.remove('active'));
-        e.currentTarget.classList.add('active');
-    }));   
+
+console.clear();
+    
+const list = document.querySelectorAll('.list');
+const nav = document.querySelector('.navigation');
+const paragraphs = document.querySelectorAll('.icon-nav p');
+
+list.forEach((item, index) => item.addEventListener('click', function(e) {
+        list.forEach(li => {
+            li.classList.remove('active');
+        });
+        paragraphs.forEach(paragraph => {
+            paragraph.classList.remove('hidden');
+        });
+        const isActive = item.classList.contains('active');
+        if (!isActive) {
+            item.classList.add('active');
+            paragraphs[index].classList.add('hidden');
+        }
+    }));
 });
 
